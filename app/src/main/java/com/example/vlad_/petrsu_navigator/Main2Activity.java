100d848b96e92b92651925e68642ca8b1baed40e
Aleks1795 <37011602+Aleks1795@users.noreply.github.com>
2018-12-24 09:54:18 +0300

    News
    Added News RSS Parcer

--- app/src/main/java/com/example/vlad_/petrsu_navigator/Main2Activity.java
+++ app/src/main/java/com/example/vlad_/petrsu_navigator/Main2Activity.java
@@ -2,12 +2,72 @@ package com.example.vlad_.petrsu_navigator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class Main2Activity extends AppCompatActivity {
+
+    String rssResult = "";
+    Boolean item = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView rss = findViewById(R.id.rss);
+        try {
+            URL rssUrl = new URL("https://petrsu.ru/rss");
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            XMLReader xmlReader = saxParser.getXMLReader();
+            RSSHandler rssHandler = new RSSHandler();
+            xmlReader.setContentHandler(rssHandler);
+            InputSource inputSource = new InputSource(rssUrl.openStream());
+            xmlReader.parse(inputSource);
+            rss.setText(rssResult);
+        } catch (IOException e) {rss.setText(e.getMessage());
+        } catch (SAXException e) {rss.setText(e.getMessage());
+        } catch (ParserConfigurationException e) {rss.setText(e.getMessage());
+        }
+    }
+
+    //Обработчик (парсер) rss ленты
+    private class RSSHandler extends DefaultHandler {
+
+        public void startElement(String uri, String localName, String qName,
+                                 Attributes attrs) throws SAXException {
+            if (localName.equals("item"))
+                item = true;
+
+            if (!localName.equals("item") && item == true)
+                rssResult = rssResult + localName + ": ";
+
+        }
+
+        public void endElement(String namespaceURI, String localName,
+                               String qName) throws SAXException {
+
+        }
+
+        public void characters(char[] ch, int start, int length)
+                throws SAXException {
+            String cdata = new String(ch, start, length);
+            if (item == true)
+                rssResult = rssResult +(cdata.trim()).replaceAll("\\s+", " ")+"\t";
+
+        }
     }
 }
--- app/src/main/java/com/example/vlad_/petrsu_navigator/MainActivity.java
+++ app/src/main/java/com/example/vlad_/petrsu_navigator/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,20 +14,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,15 +51,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
+
 
     @Override
     public void onBackPressed() {
